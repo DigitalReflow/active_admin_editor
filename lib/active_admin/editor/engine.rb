ac9e4bf729541e35edd7023d7a60384ffb29e1a5
Dan Mitchell <dan@digitalreflow.co.uk>
2018-04-18 15:32:34 +0100

    Remove deprecated register_javascript and register_stylesheet

--- lib/active_admin/editor/engine.rb
+++ lib/active_admin/editor/engine.rb
@@ -7,20 +7,11 @@ module ActiveAdmin
         def editor; ActiveAdmin::Editor.configuration end
       end
 
-      initializer 'active_admin.editor', :group => :all do |app|
+      initializer 'active_admin.editor', group: :all do |app|
         app.config.assets.precompile += [
           'active_admin/editor.js',
           'active_admin/editor.css'
         ] + ActiveAdmin::Editor.configuration.stylesheets
-
-        ActiveAdmin.application.tap do |config|
-          config.register_javascript 'active_admin/editor.js'
-          config.register_stylesheet 'active_admin/editor.css'
-
-          ActiveAdmin::Editor.configuration.stylesheets.each do |stylesheet|
-            config.register_stylesheet stylesheet
-          end
-        end
       end
     end
   end
